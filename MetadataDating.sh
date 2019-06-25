9030c79e7a3576148ea57108e0f692efea274439
Robert Jansma <r.jansma.nl@gmail.com>
2019-06-25 02:00:35 +0200

    Make sure new run overwrites old results
    To be sure

--- MetadataDating.sh
+++ MetadataDating.sh
@@ -28,8 +28,8 @@ mkdir -p "$resultDir"
 
 datasetDir="$(cd "$1" && pwd -P)"
 
-find $datasetDir -mindepth 0 -maxdepth 0 -type d  -printf "%p\t%A+\t%T+\t%C+\t%A@\t%T@\t%C@\n" > "${resultsDir}/MACTimes.tsv"
-find $datasetDir -mindepth 1 -maxdepth 1 -type d > "${resultDir}/dirsToCheck.txt"
+find $datasetDir -mindepth 0 -maxdepth 0 -type d  -printf "%p\t%A+\t%T+\t%C+\t%A@\t%T@\t%C@\n" >| "${resultDir}/MACTimes.tsv"
+find $datasetDir -mindepth 1 -maxdepth 1 -type d >| "${resultDir}/dirsToCheck.txt"
 
 while [ -s "${resultDir}/dirsToCheck.txt" ];
 do
@@ -38,18 +38,18 @@ done
 
 find $datasetDir -type f -printf "%p\t%A+\t%T+\t%C+\t%A@\t%T@\t%C@\n" >> "${resultDir}/MACTimes.tsv"
 
-cut -f1 "${resultDir}/MACTimes.tsv" | xargs -d "\n" stat --printf "%w\n" > "${resultDir}/BirthTimes.tsv"
+cut -f1 "${resultDir}/MACTimes.tsv" | xargs -d "\n" stat --printf "%w\n" >| "${resultDir}/BirthTimes.tsv"
 
-paste -d"\t" "${resultDir}/MACTimes.tsv" "${resultDir}/BirthTimes.tsv" > "${resultDir}/MACBTimes.tsv"
+paste -d"\t" "${resultDir}/MACTimes.tsv" "${resultDir}/BirthTimes.tsv" >| "${resultDir}/MACBTimes.tsv"
 
-cut -f1 "${resultDir}/MACTimes.tsv" |sed 's|.*/||'| awk -F. '{print (NF>1?$NF:"")}' > "${resultDir}/extensions.tsv"
+cut -f1 "${resultDir}/MACTimes.tsv" |sed 's|.*/||'| awk -F. '{print (NF>1?$NF:"")}' >| "${resultDir}/extensions.tsv"
 
-echo -e "File\tAccess Time\tModify Time\tChange Time\tAccess Time(epoch)\tModify Time(epoch)\tChange Time(epoch)\tBirth Time\tExtension" > "${resultDir}/results.tsv"
+echo -e "File\tAccess Time\tModify Time\tChange Time\tAccess Time(epoch)\tModify Time(epoch)\tChange Time(epoch)\tBirth Time\tExtension" >| "${resultDir}/results.tsv"
 paste -d"\t" "${resultDir}/MACBTimes.tsv" "${resultDir}/extensions.tsv" >> "${resultDir}/results.tsv"
 
 if [ ! -z "$3" ]
 then
-	awk -F"\t" 'FNR==NR{k[$1]=1;next;} FNR==1 || k[$9]' "${3}" "${resultDir}/results.tsv" > "${resultDir}/subsetResults.tsv"
+	awk -F"\t" 'FNR==NR{k[$1]=1;next;} FNR==1 || k[$9]' "${3}" "${resultDir}/results.tsv" >| "${resultDir}/subsetResults.tsv"
 fi
 
 
